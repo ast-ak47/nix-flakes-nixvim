0961c7222c28ca7b6d42ae37537644df3a9131e2
ast-ak47 <s2511142@u.tsukuba.ac.jp>
2026-03-12 03:23:07 +0900

    Add theme config

---
+++ config/default.nix
@@ -0,0 +1,5 @@
+{
+  imports = [
+    ./lib/ui/theme.nix
+  ];
+}
---
+++ config/lib/ui/theme.nix
@@ -0,0 +1,14 @@
+{
+  # One Dark Pro color scheme
+  # ref: https://github.com/olimorris/onedarkpro.nvim
+  colorschemes.onedarkpro = {
+    enable = true;
+    settings = {
+      options = {
+        transparency = true;
+	terminal_colors = true;
+	cursorline = true;
+      };
+    };
+  };
+}
